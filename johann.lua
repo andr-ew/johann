e7341c3d3d9e7b2c06add737243f9560cc215189
andr-ew <andrewcshike@gmail.com>
2022-01-11 22:44:16 -0600

    norns works !!!

---
+++ johann.lua
@@ -0,0 +1,23 @@
+-- johann
+
+engine.name = "Johann"
+
+m = midi.connect()
+m.event = function(data)
+    local msg = midi.to_msg(data)
+    if msg.type == "note_on" then
+        
+        -- args: midival, dynamic, variation, release
+        engine.noteOn(msg.note, 1, 1, 0)
+    elseif msg.type == "note_off" then
+    end
+end
+
+function init()
+    -- send the engine a folder of samples, naming format is the same as mx.samples
+
+    engine.loadfolder(_path.audio .. 'johann/classic')
+end
+
+function cleanup() 
+end

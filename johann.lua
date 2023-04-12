9b7e7fd20bda0782f2686a707888f6dd7b9dba9b
andr-ew <andrewcshike@gmail.com>
2023-04-11 19:07:59 -0500

    grid root, passthrough

--- johann.lua
+++ johann.lua
@@ -14,25 +14,32 @@ engine.name = "Johann"
 
 params:add{
     id = 'level', type = 'control',
-    controlspec = cs.def{ min = 0, max = 15, default = 1 },
+    controlspec = cs.def{ min = 0, max = 15, default = 4 },
     action = function(v) engine.level(v) end,
 }
 params:add{
     id = 'rate', type = 'control',
     controlspec = cs.def{ 
-        min = -2, max = 2, default = 0, quantum = 1/100/4,
+        min = -2, max = 2, default = -0.12, quantum = 1/100/4,
     },
     action = function(v) engine.rate(2^v) end,
+}
+local notes = { 'C','C#','D','D#','E','F','F#','G','G#','A','A#','B', }
+params:add{
+    id = 'grid root', type = 'option',
+    options = notes,
 }
 
 m = midi.connect()
 m.event = function(data)
-    local msg = midi.to_msg(data)
-    if msg.type == "note_on" then
-        
-        -- args: midival, dynamic, variation, release
-        engine.noteOn(msg.note-12, math.ceil((msg.vel/127)*7), 1, 0)
-    elseif msg.type == "note_off" then
+    if not get_passthrough() then
+        local msg = midi.to_msg(data)
+        if msg.type == "note_on" then
+            
+            -- args: midival, dynamic, variation, release
+            engine.noteOn(msg.note-12, math.ceil((msg.vel/127)*7), 1, 0)
+        elseif msg.type == "note_off" then
+        end
     end
 end
 
@@ -48,6 +55,60 @@ end
 function cleanup() 
     params:write()
 end
+
+function get_passthrough()
+    return params:get('monitor_level') > -inf
+end
+
+function set_passthrough(v)
+    params:set('monitor_level', v and 0.0 or -inf)
+    redraw()
+end
+
+--norns ui
+
+x = {}
+y = {}
+
+local mar = { left = 2, top = 7, right = 2, bottom = 0 }
+local w = 128 - mar.left - mar.right
+local h = 64 - mar.top - mar.bottom
+
+x[1] = mar.left
+x[2] = 128/2
+y[1] = mar.top
+y[2] = mar.top + h*(1.5/8)
+y[3] = mar.top + h*(5.5/8)
+y[4] = mar.top + h*(7/8)
+
+local e = {
+    { x = x[1], y = y[1] },
+    { x = x[1], y = y[3] },
+    { x = x[2], y = y[3] },
+}
+local k = {
+    { x = x[1], y = y[2] },
+    { x = x[1], y = y[4] },
+    { x = x[2], y = y[4] },
+}
+
+function redraw()
+    screen.clear()
+
+    screen.move(k[2].x, k[2].y)
+    screen.level(get_passthrough() and 15 or 4)
+    screen.text('passthrough')
+        
+    screen.update()
+end
+
+function key(n, z)
+    if z==1 and n==2 then
+        set_passthrough(not get_passthrough())        
+    end
+end
+
+--grid ui
 
 local root = 48
 local scale = { 0, 2, 4, 7, 9 } --maj pent
@@ -93,7 +154,7 @@ local function App()
                 local deg = (k.x-1)%#iv+1
 
                 --local hz = root * 2^oct * 2^(deg/12)
-                local note = root + (oct*12) + iv[deg] - 1
+                local note = root + (params:get('grid root') - 1) + (oct*12) + iv[deg] - 1
 
                 if add then 
                     local dyn = forte > 0 and 5 or (
